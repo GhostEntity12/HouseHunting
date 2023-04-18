3b8db2746fd2e78cab6f23561d31438a3479f20e
Kao Kai Siang <103819380@student.swin.edu.au>
2023-04-18 11:59:34 +1000

    Added loading furniture to house scene

--- Assets/Scripts/HouseManager.cs
+++ Assets/Scripts/HouseManager.cs
@@ -1,10 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class HouseManager : MonoBehaviour
+public class HouseManager : MonoBehaviour, IDataPersistence
 {
     private static HouseManager instance;
+    private List<SerializableDecoration> serializedDecorations;
 
     public static HouseManager Instance { get; private set; }
+
+    public void LoadData(GameData data)
+    {
+        serializedDecorations = data.serializedDecorations;
+    }
+
+    public void SaveData(GameData data)
+    {
+    }
 
     private void Awake() 
     {
@@ -12,9 +23,18 @@ public class HouseManager : MonoBehaviour
             Destroy(this.gameObject);
         else
             Instance = this;
+
+        serializedDecorations = new List<SerializableDecoration>();
     }
 
     private void Start() 
     {
+        List<(PlaceableSO placeableSO, SerializableDecoration serializableDecoration)> placeableSOsMap = SerializableDecoration.Deserialize(serializedDecorations);
+        foreach ((PlaceableSO placeableSO, SerializableDecoration serializableDecoration) in placeableSOsMap)
+        {
+            Placeable spawnedPlaceable = Instantiate(placeableSO.placeablePrefab);
+            spawnedPlaceable.transform.position = serializableDecoration.position;
+            spawnedPlaceable.RotateToAngle(serializableDecoration.rotationAngle);
+        }
     }
 }
