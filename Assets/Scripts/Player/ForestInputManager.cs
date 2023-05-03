e6ff192707c03338a2b77e53c4a55fc3cc0a1547
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-05-03 23:55:39 +1000

    Optimised some functions
    Implemented usage of TryGetComponent over a GetComponent and null check

--- Assets/Scripts/Player/ForestInputManager.cs
+++ Assets/Scripts/Player/ForestInputManager.cs
@@ -20,47 +20,39 @@ public class ForestInputManager : Singleton<ForestInputManager>
 		playerInput.Forest.Interact.performed += ctx => Interact();
 	}
 
-        playerInput.Forest.Interact.performed += ctx => Interact();
-    }
+	private void FixedUpdate()
+	{
+		movement.Move(playerInput.Forest.Movement.ReadValue<Vector2>());
+	}
 
-    private void FixedUpdate()
-    {
-        movement.Move(playerInput.Forest.Movement.ReadValue<Vector2>());
-    }
+	private void LateUpdate()
+	{
+		look.Look(playerInput.Forest.Look.ReadValue<Vector2>());
+	}
 
-    private void LateUpdate() 
-    {
-        look.Look(playerInput.Forest.Look.ReadValue<Vector2>());
-    }
+	private void OnEnable()
+	{
+		playerInput.Forest.Enable();
+	}
 
-    private void OnEnable()
-    {
-        playerInput.Forest.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerInput.Forest.Disable();
-    }
+	private void OnDisable()
+	{
+		playerInput.Forest.Disable();
+	}
 
     private void Interact()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 3f))
-        {
-            Shootable shootable = hit.transform.GetComponentInParent<Shootable>();
-            if (shootable != null)
-            {
-                if (shootable.IsDead)
-                {
-                    ForestManager.Instance.HuntingInventory.AddItem(shootable.GetInventoryItem());
-                    Debug.Log(ForestManager.Instance.HuntingInventory);
-                    Destroy(shootable.gameObject);
-                }
-            }
-            //if we are interacting with a door, load the house scene
-            if (hit.transform.parent.transform.CompareTag("Door"))
-                ForestManager.Instance.RespawnInHouse();
-        }
-    }
+		if (Physics.Raycast(camera.transform.position, camera.transform.forward, out RaycastHit hit, 3f))
+		{
+			if (hit.transform.TryGetComponent(out Shootable shootable) && shootable.IsDead)
+			{
+				ForestManager.Instance.HuntingInventory.AddItem(shootable.GetInventoryItem());
+				Debug.Log(ForestManager.Instance.HuntingInventory);
+				Destroy(shootable.gameObject);
+			}
+			//if we are interacting with a door, load the house scene
+			if (hit.transform.parent.transform.CompareTag("Door"))
+				ForestManager.Instance.RespawnInHouse();
+		}
+	}
 }
