20d19bb00363e3d0a45b2abb2fdab8ceda03a0ac
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-05-05 12:24:38 +1000

    Implemented spawning mechanics

---
+++ Assets/Scripts/SpawnerManger.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManger : MonoBehaviour
+{
+	List<Spawner> spawners = new();
+
+	// This can be swapped with the percentageOfSpawnersToTrigger below if wanted
+	[SerializeField] int numToSpawn;
+
+	//[Range(0, 1)]
+	//[SerializeField] float percentageOfSpawnersToTrigger;
+
+	private void Awake()
+	{
+		spawners = new List<Spawner>(FindObjectsOfType<Spawner>());
+		
+		//numToSpawn = Mathf.FloorToInt(spawners.Count * percentageOfSpawnersToTrigger);
+
+		// Warn if there aren't enough spawners
+		if (spawners.Count < numToSpawn)
+		{
+			Debug.LogWarning($"There may not be enough spawners - {spawners.Count} spawners were found, a minimum of {numToSpawn} are suggested.");
+			numToSpawn = spawners.Count;
+		}
+
+		spawners.Shuffle();
+		Queue<Spawner> spawnerQueue = new(spawners);
+		for (int i = 0; i < numToSpawn; i++)
+		{
+			if (spawnerQueue.Count == 0)
+			{
+				Debug.LogWarning("Couldn't spawn the number of requested furniture.");
+				break;
+			}
+			else if (!spawnerQueue.Dequeue().Spawn())
+			{
+				// Failed spawn
+				i--;
+			}
+		}
+	}
+}
