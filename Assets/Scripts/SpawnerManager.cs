3c66e327c1c5f20445a4217b1900d889ed11944b
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-05-10 15:52:02 +1000

    Fixed a naming issue

--- Assets/Scripts/SpawnerManager.cs
+++ Assets/Scripts/SpawnerManager.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnerManger : MonoBehaviour
+public class SpawnerManager : MonoBehaviour
 {
 	List<Spawner> spawners = new();
 
