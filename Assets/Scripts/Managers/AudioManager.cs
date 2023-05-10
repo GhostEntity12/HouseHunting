bbc82d3ac368b3918c5f74ebced8991906358d61
SassDude <patsiassios@gmail.com>
2023-05-10 16:28:13 +1000

    created audio manager script

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+    void Awake()
+    {
+        if (instance == null) 
+            instance = this;
+        else 
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.playOnAwake = s.playOnAwake;
+            
+            //s.source.outputAudioMixerGroup = s.mixerGroup;
+        }
+    }
+
+    void Start()
+    {
+        Play("Ambience");
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Sound s = System.Array.Find(sounds, sound => sound.name == name);
+        if (s == null) 
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = System.Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Stop();
+    }
+
+    public void Pause(string name)
+    {
+        Sound s = System.Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Pause();
+    }
+}
