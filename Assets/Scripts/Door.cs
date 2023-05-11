944ba65d61a340dc5666ba3eedccd13f2ed7cb8a
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-05-12 03:04:42 +1000

    Playtest prep

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,16 +6,10 @@ public class Door : MonoBehaviour, IInteractable
     [SerializeField] private Object sceneToLoad;
 
     public void Interact()
-    {
-        if (sceneToLoad != null)
-        {
-            // if we are in hunting scene, call respawn in house instead
-            if (HuntingManager.Instance != null)
-                HuntingManager.Instance.RespawnInHouse();
-            else
-                SceneManager.LoadScene(sceneToLoad.name);
-        }
-        else
-            Debug.LogError("No scene to load");
+	{
+		if (HuntingManager.Instance != null)
+			HuntingManager.Instance.RespawnInHouse();
+		else
+			SceneManager.LoadScene(0);
     }
 }
