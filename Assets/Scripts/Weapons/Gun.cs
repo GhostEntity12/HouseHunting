7af984ab6e34f3e01be798326209a86466b97552
Nikki <ngolev.bus@gmail.com>
2023-08-04 16:27:22 +1000

    Update Assets/Scripts/Weapons/Gun.cs
    Co-authored-by: GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>

--- Assets/Scripts/Weapons/Gun.cs
+++ Assets/Scripts/Weapons/Gun.cs
@@ -32,7 +32,7 @@ public class Gun : MonoBehaviour
     {
         if (GameManager.Instance.IsPaused) return;
         if ( !readyToShoot || WeaponManager.Instance.BulletsInMag <= 0 || reloading) return;
-        if (HuntingInputManager.Instance.WeaponWheelisOpen()) return; // dont shoot when weapon wheel is open
+        if (HuntingInputManager.Instance.WeaponWheelIsOpen) return; // dont shoot when weapon wheel is open
 
         if (firstShot)
             soundAlerter.MakeSound(GunSO.volume, transform.position);
