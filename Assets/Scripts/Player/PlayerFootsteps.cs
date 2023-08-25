f35b24b1f1df7cbc108a79f160f5f7194039d99c
SassDude <patsiassios@gmail.com>
2023-08-25 17:46:20 +1000

    init player footsteps

---
+++ Assets/Scripts/Player/PlayerFootsteps.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    private CharacterController controller;
+    private bool isWalking;
+    private bool keysPressed;
+    private bool grounded;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        isWalking = false;
+        keysPressed = false;
+    }
+
+    void Update()
+    {
+        HandleFootsteps();
+        HandleAudio();
+        HandleLanding();
+    }
+
+    private void HandleFootsteps()
+    {
+        keysPressed = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
+    }
+
+    private void HandleAudio()
+    {
+        if (controller.isGrounded)
+        {
+            if (!isWalking && keysPressed)
+            {
+                isWalking = true;
+                AudioManager.Instance.Play("WalkOnGrass");
+            }
+            else if (isWalking && !keysPressed)
+            {
+                isWalking = false;
+                AudioManager.Instance.Stop("WalkOnGrass");
+            }
+        }
+        else if (!controller.isGrounded)
+        {
+            isWalking = false;
+            AudioManager.Instance.Pause("WalkOnGrass");
+        }
+    }
+        private void HandleLanding()
+    {
+        if (grounded != controller.isGrounded)
+        {
+            grounded = controller.isGrounded;
+            if (grounded)
+            {
+                AudioManager.Instance.Play("Landing");
+            }
+        }
+    }
+}
