0c5dd7949191207b273ca3d1e14ec53d72beb8ec
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-08-28 16:51:44 +1000

    Cleaned up SoundAlerter.cs
    No functionality changes

--- Assets/Scripts/Player/SoundAlerter.cs
+++ Assets/Scripts/Player/SoundAlerter.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundAlerter : MonoBehaviour
@@ -9,36 +7,23 @@ public class SoundAlerter : MonoBehaviour
 
     public void MakeSound(float volume, Vector3 source, float? rangeOverride = null)
     {
-        //OnSoundEmitEvent?.Invoke(volume, transform.position);
-        HashSet<WanderAI> furnitureInRange = new HashSet<WanderAI>();
-        Collider[] hitColliders;
+        Collider[] hitColliders = Physics.OverlapSphere(source, rangeOverride ?? volume);
 
-        if (rangeOverride != null)
-            hitColliders = Physics.OverlapSphere(source, (float)rangeOverride);
-        else
-            hitColliders = Physics.OverlapSphere(source, volume);
-
-        foreach (Collider hitCollider in hitColliders)
+		foreach (Collider hitCollider in hitColliders)
         {
-            WanderAI ai = hitCollider.transform.GetComponent<WanderAI>();
-            if (ai != null) 
-                furnitureInRange.Add(ai);
-        }
-
-        foreach (WanderAI ai in furnitureInRange)
-        {
-            float distance = Vector3.Distance(source, ai.transform.position); // Get the distance between the source and furniture
-            float soundFalloff; /* Get the soundFalloff based on the distance. Max distance is 0, Point blank is 1. 
+            if (hitCollider.transform.TryGetComponent(out WanderAI ai))
+            {
+				float distance = Vector3.Distance(source, ai.transform.position); // Get the distance between the source and furniture
+				float soundFalloff = rangeOverride == null
+					? Mathf.Pow((distance - volume) / -volume, 2f)
+					: Mathf.Pow((distance - (float)rangeOverride) / -(float)rangeOverride, 2f); /* Get the soundFalloff based on the distance. Max distance is 0, Point blank is 1. 
                Original function was: 
                ((distance - max) / (min - max))^2
                It has been simplified for now, but if we want to make the minimum distance to be that of the furniture's hitbox we can do that, 
                but measures must be taken so nothing bad happens if the sound came from within that hitbox.
             */
-            if (rangeOverride != null)
-                soundFalloff = Mathf.Pow((distance - (float)rangeOverride) / -(float)rangeOverride,2f);
-            else
-                soundFalloff = Mathf.Pow((distance - volume) / -volume, 2f);
-            ai.IncrementAlertness(volume * soundFalloff);
+				ai.IncrementAlertness(volume * soundFalloff);
+			}
         }
     }
 }
