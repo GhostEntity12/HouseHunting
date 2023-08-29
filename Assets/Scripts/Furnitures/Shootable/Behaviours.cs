cb8958ca2be63a67cd6eb135938fce4b30b2b648
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-08-30 00:25:23 +1000

    Removed old code and reworked AI to use draggable scriptable objects

---
+++ Assets/Scripts/Furnitures/Shootable/Behaviours.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Behaviour : ScriptableObject
+{
+	public abstract void Act(ref Knowledge knowledge);
+}
+
+public class Knowledge
+{
+	public Vector3? dangerPosition;
+
+	public Vector3 AIPosition { get; private set; }
+	public Vector3 AIForward {get; private set; }
+	public Vector3 PlayerPosition {get; private set; }
+	public FurnitureSO Stats {get; private set; }
+	public NavMeshAgent Agent {get; private set; }
+	public SoundAlert MostProminentSound {get; private set; }
+	public bool CanSeePlayer {get; private set; }
+
+	public Knowledge(Vector3 t, Vector3 f, Vector3 p, Vector3? d, FurnitureSO s, NavMeshAgent a, SoundAlert sa, bool v)
+	{
+		AIPosition = t;
+		AIForward = f;
+		PlayerPosition = p;
+		dangerPosition = d;
+		Stats = s;
+		Agent = a;
+		MostProminentSound = sa;
+		CanSeePlayer = v;
+	}
+}
+
+[CreateAssetMenu(fileName = "Roam Behaviour", menuName = "Behaviours/Roam")]
+public class RoamBehaviour : Behaviour
+{
+	public override void Act(ref Knowledge knowledge)
+	{
+		// If the agent is close to it's destination, choose a new one
+		if (knowledge.Agent.remainingDistance < 1)
+		{
+			// Get a point in front of the furniture, and a random point
+			// in a circle centered on that point, then navigate to it
+			// This favours the AI moving generally forward
+			if (WanderAI.RandomPoint(knowledge.AIPosition + knowledge.AIForward * 5, 3, out Vector3 newPointForward))
+			{
+				knowledge.Agent.SetDestination(newPointForward);
+			}
+			// If a point can't be found, then get a new point centered 
+			// on the AI. This lets the AI choose a new random forward
+			// An intemediary step could be added where it tries to reverse first
+			else if (WanderAI.RandomPoint(knowledge.AIPosition, 10, out Vector3 newPoint))
+			{
+				knowledge.Agent.SetDestination(newPoint);
+			}
+		}
+	}
+}
+
+[CreateAssetMenu(fileName = "Search Behaviour", menuName = "Behaviours/Search")]
+public class SearchBehaviour : Behaviour
+{
+	public override void Act(ref Knowledge knowledge)
+	{
+		if (knowledge.CanSeePlayer)
+		{
+			// Cache the player's position in case it loses sight of the player
+			knowledge.dangerPosition = knowledge.PlayerPosition;
+		}
+		//else if (knowledge.mostProminentSound.volume > 0)
+		//{
+		//	knowledge.dangerPosition = knowledge.mostProminentSound.position;
+		//}
+		else if (knowledge.dangerPosition == null)
+		{
+			// Hasn't yet seen and can't see the player in this state.
+			// Look around randomly
+		}
+
+		// Look at dangerPosition
+	}
+}
+
+[CreateAssetMenu(fileName = "Flee Behaviour", menuName = "Behaviours/Flee General")]
+public class FleeBehaviour : Behaviour
+{
+	public override void Act(ref Knowledge knowledge)
+	{
+		// Update dangerPosition if required
+		if (knowledge.CanSeePlayer)
+		{
+			knowledge.dangerPosition = knowledge.PlayerPosition;
+		}
+		else if (knowledge.MostProminentSound.volume > 0)
+		{
+			knowledge.dangerPosition = knowledge.MostProminentSound.position;
+		}
+
+		// If we reached the end of our current path or a new sound has been detected, generate a new path
+		if (knowledge.Agent.remainingDistance < 1 && WanderAI.RandomPoint(WanderAI.FindFleePoint((Vector3)knowledge.dangerPosition, knowledge.AIPosition), 2, out Vector3 fleeDestination))
+		{
+			knowledge.Agent.SetDestination(fleeDestination);
+		}
+	}
+}
+
+[CreateAssetMenu(fileName = "Flee Player Behaviour", menuName = "Behaviours/Flee Player")]
+public class FleePlayerBehaviour : Behaviour
+{
+	public override void Act(ref Knowledge knowledge)
+	{
+		if (knowledge.Agent.remainingDistance < 1 && WanderAI.RandomPoint(WanderAI.FindFleePoint(knowledge.PlayerPosition, knowledge.AIPosition), 2, out Vector3 fleeDestination))
+		{
+			knowledge.Agent.SetDestination(fleeDestination);
+		}
+	}
+}
--- Assets/Scripts/Furnitures/Shootable/WanderAI.cs
+++ Assets/Scripts/Furnitures/Shootable/WanderAI.cs
@@ -6,39 +6,32 @@ using UnityEditor;
 
 public class WanderAI : MonoBehaviour
 {
-	const float aiEntityDistance = 100f; // This distance will be used to determine whether or not the AI should run or not.
-										 //enum PathfindingState { Fleeing, Chasing, Investigating, Looking, Wandering }
 	enum AlertLevels { None, Level1, Level2, Level3 }
+										 //enum PathfindingState { Fleeing, Chasing, Investigating, Looking, Wandering }
+	const float aiEntityDistance = 100f; // This distance will be used to determine whether or not the AI should run or not.
+
+	[SerializeField] private FurnitureSO stats;
+	[SerializeField] private Behaviour threshold0Behaviour;
+	[SerializeField] private Behaviour threshold1Behaviour;
+	[SerializeField] private Behaviour threshold2Behaviour;
+	[SerializeField] private Behaviour threshold3Behaviour;
 
 	private Shootable shootable;
 	private Canvas alertCanvas;
-	//private PathfindingState pathfindingState = PathfindingState.Wandering;
 	private AlertLevels currentBehaviour = AlertLevels.None;
-	//private float timeSinceLastAttack = 0f;
-	//private float timeSinceLastBump = 0f;
 	private bool isRelaxed;
 	private float relaxTimer = 0f;
 	private Vector3? dangerPosition = null;
-
-	//private readonly float fleeingSearchRange = 3.0f;
-	//private readonly float investigateRadius = 2f;
-	//private readonly float alertDistance = 30f;
-	//private readonly float timePerBump = 1f; // This is used to determine if the AI pathfinding should recalculate itself, so that it can properly escape the player.
-
 	private NavMeshAgent agent;
-	//public float wanderRadius = 15f; // how far the AI can wander
-
 	private readonly Queue<SoundAlert> sounds = new();
 	private Transform player;
-	private FurnitureSO stats;
-
 	private float alertness = 0;
+
 	public float Alertness
 	{
 		get => alertness;
 		private set => alertness = Mathf.Clamp(value, 0, 100);
 	}
-
 	public bool IsAggressive => stats.behavior != AIType.Prey;
 
 	private void Awake()
@@ -46,7 +39,10 @@ public class WanderAI : MonoBehaviour
 		agent = GetComponent<NavMeshAgent>();
 		shootable = GetComponent<Shootable>();
 		alertCanvas = GetComponentInChildren<Canvas>();
+	}
 
+	private void Start()
+	{
 		// Cache some values
 		player = HuntingManager.Instance.Player;
 		stats = shootable.FurnitureSO;
@@ -76,40 +72,59 @@ public class WanderAI : MonoBehaviour
 		// Change alertness based on whether the player is visible
 		UpdateSightAlertness(canSeePlayer);
 
+		// Bundle information to pass to behaviours
+		Knowledge k = new(transform.position, transform.forward, player.position, dangerPosition, stats, agent, sound, canSeePlayer);
+
 		// State machine
 		switch (currentBehaviour)
 		{
 			case AlertLevels.None:
-				Threshold0();
+				Threshold0(k);
 				break;
 			case AlertLevels.Level1:
-				Threshold1(canSeePlayer);
+				Threshold1(k);
 				break;
 			case AlertLevels.Level2:
-				Threshold2(canSeePlayer, sound);
+				Threshold2(k);
 				break;
 			case AlertLevels.Level3:
-				Threshold3();
+				Threshold3(k);
 				break;
 		}
+
+		// Update the cached danger position in case the behaviours changed it.
+		dangerPosition = k.dangerPosition;
 
 		// This is debug
 		if (Input.GetKeyDown(KeyCode.T))
 		{
 			ToggleDoesRotate();
 		}
+
 	}
 
-	private void Threshold0()
+	private void OnDrawGizmos()
+	{
+		if (stats.senses.Length == 0 || !Application.isPlaying) return;
+		foreach (ViewConeSO cone in stats.senses)
+		{
+			Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
+			cone.DebugDraw(transform, player.position, 0.5f);
+			Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
+			cone.DebugDraw(transform, player.position, 0.2f);
+		}
+	}
+
+	private void Threshold0(Knowledge knowledge)
 	{
 		// Update behaviour level where appropriate
-		if (Alertness > 33)
+		if (Alertness > stats.alertnessThreshold1)
 			TransitionToThreshold1();
 
-		Roam();
+		threshold0Behaviour.Act(ref knowledge);
 	}
 
-	private void Threshold1(bool playerVisible)//, SoundAlert sound)
+	private void Threshold1(Knowledge knowledge)
 	{
 		// Update behaviour level where appropriate
 		switch (Alertness)
@@ -117,80 +132,42 @@ public class WanderAI : MonoBehaviour
 			case 0:
 				TransitionToThreshold0();
 				break;
-			case > 66:
+			case float a when a >= stats.alertnessThreshold2:
 				TransitionToThreshold2();
 				break;
 		}
-
-		if (playerVisible)
-		{
-			// Cache the player's position in case it loses sight of the player
-			dangerPosition = player.position;
-
-			// Look at the player
-		}
-		else if (dangerPosition == null)
-		{
-			// Hasn't yet seen and can't see the player in this state.
-			// Unsure if this is going to be added
-			//if (sound.volume > 0)
-			//{
-			//	dangerPosition = sound.position;
-			//}
-			// Look around randomly
-		}
-		else
-		{
-			// Look at dangerPosition
-		}
+		threshold1Behaviour.Act(ref knowledge);
 	}
 
-
-	private void Threshold2(bool playerVisible, SoundAlert sound)
+	private void Threshold2(Knowledge knowledge)
 	{
 		// Update behaviour level where appropriate
 		switch (Alertness)
 		{
-			case < 33:
+			case float a when a >= stats.alertnessThreshold1:
 				TransitionToThreshold1();
 				break;
-			case 100:
+			case float a when a >= stats.alertnessThreshold3:
 				TransitionToThreshold3();
 				break;
 		}
-
-		// Update dangerPosition if required
-		if (playerVisible)
-		{
-			dangerPosition = player.position;
-		}
-		else if (sound.volume > 0)
-		{
-			dangerPosition = sound.position;
-		}
-
-		// If we reached the end of our current path or a new sound has been detected, generate a new path
-
+		threshold2Behaviour.Act(ref knowledge);
 	}
 
-	private void Threshold3()
+	private void Threshold3(Knowledge knowledge)
 	{
 		// Update behaviour level where appropriate
 		switch (Alertness)
 		{
-			case < 33:
+			case float a when a < stats.alertnessThreshold1:
 				TransitionToThreshold1();
 				break;
-			case 100:
+			case float a when a > stats.alertnessThreshold3:
 				TransitionToThreshold3();
 				break;
 		}
 
-		dangerPosition = player.position;
-		if (agent.remainingDistance < 1 && RandomPoint(FindFleePoint((Vector3)dangerPosition), 2, out Vector3 fleeDestination))
-		{
-			agent.SetDestination(fleeDestination);
-		}
+		threshold3Behaviour.Act(ref knowledge);
 	}
 
 	void TransitionToThreshold0()
@@ -198,7 +175,7 @@ public class WanderAI : MonoBehaviour
 		currentBehaviour = AlertLevels.None;
 	}
 
-	void TransitionToThreshold1()
+	private void TransitionToThreshold1()
 	{
 		// Clear the path
 		agent.ResetPath();
@@ -210,24 +187,20 @@ public class WanderAI : MonoBehaviour
 		agent.speed = stats.speed;
 		currentBehaviour = AlertLevels.Level1;
 	}
-	void TransitionToThreshold2()
+	
+	private void TransitionToThreshold2()
 	{
 		// Release control of rotation
 		agent.updateRotation = true;
 		agent.speed = stats.speed;
 		currentBehaviour = AlertLevels.Level2;
 	}
-	void TransitionToThreshold3()
+	
+	private void TransitionToThreshold3()
 	{
 		agent.speed = stats.speed * 2;
 		currentBehaviour = AlertLevels.Level3;
 	}
-
-	/// <summary>
-	/// Gets the average sense distance
-	/// </summary>
-	/// <returns></returns>
-	private float AverageSightDist() => stats.senses.Average(vc => vc.length);
 
 	/// <summary>
 	/// Calculate whether the furniture can see the player
@@ -262,45 +235,24 @@ public class WanderAI : MonoBehaviour
 	/// Returns a point distance units away in the opposite direction from fleeFrom
 	/// </summary>
 	/// <param name="fleeFrom"></param>
+	/// <param name="currentPosition"></param>
 	/// <param name="distance"></param>
-	private Vector3 FindFleePoint(Vector3 fleeFrom, float distance = 3)
+	public static Vector3 FindFleePoint(Vector3 fleeFrom, Vector3 currentPosition, float distance = 3)
 	{
 		// Flatten the fleeFrom to be level with the AI
-		fleeFrom.y = transform.position.y;
+		fleeFrom.y = currentPosition.y;
 
 		// Calculate the position to flee to
-		Vector3 fleeTo = transform.position + ((transform.position - fleeFrom).normalized * distance);
+		Vector3 fleeTo = currentPosition + ((currentPosition - fleeFrom).normalized * distance);
 		// Debug draws
-		Debug.DrawLine(fleeFrom, transform.position, Color.cyan, 15);
-		Debug.DrawLine(transform.position, fleeTo, Color.blue, 15);
+		Debug.DrawLine(fleeFrom, currentPosition, Color.cyan, 15);
+		Debug.DrawLine(currentPosition, fleeTo, Color.blue, 15);
 		Debug.DrawLine(fleeTo, fleeTo + Vector3.up * 10, Color.red, 15);
 
 		return fleeTo;
 	}
 
-	private void Roam()
-	{
-		// If the agent is close to it's destination, choose a new one
-		if (agent.remainingDistance < 1)
-		{
-			// Get a point in front of the furniture, and a random point
-			// in a circle centered on that point, then navigate to it
-			// This favours the AI moving generally forward
-			if (RandomPoint(transform.position + transform.forward * 5, 3, out Vector3 newPointForward))
-			{
-				agent.SetDestination(newPointForward);
-			}
-			// If a point can't be found, then get a new point centered 
-			// on the AI. This lets the AI choose a new random forward
-			// An intemediary step could be added where it tries to reverse first
-			else if (RandomPoint(transform.position, 10, out Vector3 newPoint))
-			{
-				agent.SetDestination(newPoint);
-			}
-		}
-	}
-
-	private bool RandomPoint(Vector3 center, float range, out Vector3 result)
+	public static bool RandomPoint(Vector3 center, float range, out Vector3 result)
 	{
 		Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
 		if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
@@ -340,16 +292,10 @@ public class WanderAI : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Adds a sound to be processed on the next frame
-	/// </summary>
-	/// <param name="sound"></param>
-	public void EnqueueSound(SoundAlert sound) => sounds.Enqueue(sound);
-
-	/// <summary>
 	/// Iterates through the queue of sounds and adds their volume to the furniture's alertness 
 	/// </summary>
 	/// <param name="loudestSound">The sound with the largest volume</param>
-	void ProcessSounds(out SoundAlert loudestSound)
+	private void ProcessSounds(out SoundAlert loudestSound)
 	{
 		loudestSound = new(Vector3.zero, 0);
 		while (sounds.Count > 0)
@@ -363,205 +309,13 @@ public class WanderAI : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Adds a sound to be processed on the next frame
+	/// </summary>
+	/// <param name="sound"></param>
+	public void EnqueueSound(SoundAlert sound) => sounds.Enqueue(sound);
+
 	[ContextMenu("Toggle Does Rot")]
 	public void ToggleDoesRotate() => agent.updateRotation = !agent.updateRotation;
 
-	private void OnDrawGizmos()
-	{
-		if (stats.senses.Length == 0 || !Application.isPlaying) return;
-		foreach (ViewConeSO cone in stats.senses)
-		{
-			Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
-			cone.DebugDraw(transform, player.position, 0.5f);
-			Handles.zTest = UnityEngine.Rendering.CompareFunction.Greater;
-			cone.DebugDraw(transform, player.position, 0.2f);
-		}
-	}
-
-	// TODO: Rework this
-	//private void PredatorAI()
-	//{
-	//	float distance = Vector3.Distance(transform.position, player.transform.position);
-	//
-	//	if (distance <= 2f)
-	//	{
-	//		agent.isStopped = true;
-	//		if (Alertness >= stats.alertnessThreshold3)
-	//		{
-	//			AttackPlayer();
-	//		}
-	//	}
-	//	else
-	//	{
-	//		agent.isStopped = false;
-	//		agent.SetDestination(player.transform.position);
-	//		pathfindingState = PathfindingState.Chasing;
-	//	}
-	//}
-	//
-	//private void PreyAI(bool useOld = true)
-	//{
-	//	timeSinceLastBump += Time.deltaTime;
-	//	bool hasBumped = false;
-	//	if (timeSinceLastBump > timePerBump && Vector3.Distance(player.transform.position, transform.position) < agent.radius * 4)
-	//	{
-	//		hasBumped = true;
-	//		timeSinceLastBump = 0f;
-	//		Debug.Log("A Threat just touched me!");
-	//	}
-	//	// Keep fleeing if you're at the last pathfinding destination, or start fleeing if you weren't, or rethink your escape plan because you just bumped into a human!
-	//	if (agent.remainingDistance <= agent.stoppingDistance || pathfindingState != PathfindingState.Fleeing || hasBumped)
-	//	{
-	//		// Plan A of escaping the dreadful human: Run directly away from the human!
-	//		Debug.Log("Threat Detected! Initiate Escape Plan A!");
-	//		Vector3 playerDirection = (player.transform.position - transform.position).normalized;
-	//		Vector3 destination = transform.position - (playerDirection * (AverageSightDist() + fleeingSearchRange));
-	//		if (NavMesh.SamplePosition(destination, out NavMeshHit hit, fleeingSearchRange, NavMesh.AllAreas))
-	//		{
-	//			agent.SetDestination(hit.position);
-	//			pathfindingState = PathfindingState.Fleeing;
-	//		}
-	//		else
-	//		{
-	//			// If that's impossible to do without staying in-range, enter Plan B of escaping the dreadful human: Run sideways away from the human!
-	//			Debug.Log("Plan A Failed! Initiate Escape Plan B!");
-	//			Vector3 leftDirection = Quaternion.AngleAxis(90, Vector3.up) * playerDirection;
-	//			Vector3 leftDestination = transform.position - (leftDirection * (AverageSightDist() + fleeingSearchRange));
-	//			Vector3 rightDirection = Quaternion.AngleAxis(-90, Vector3.up) * playerDirection;
-	//			Vector3 rightDestination = transform.position - (rightDirection * (AverageSightDist() + fleeingSearchRange));
-	//			// Choose the most ideal direction to get far away from the human!
-	//			destination = Vector3.Distance(player.transform.position, leftDestination) < Vector3.Distance(player.position, rightDestination)
-	//				? leftDestination
-	//				: rightDestination;
-	//			if (NavMesh.SamplePosition(destination, out NavMeshHit hit1, fleeingSearchRange, NavMesh.AllAreas))
-	//			{
-	//				agent.SetDestination(hit1.position);
-	//				pathfindingState = PathfindingState.Fleeing;
-	//			}
-	//			else
-	//			{
-	//				// If that direction cannot get far enough, try the other direction!
-	//				Debug.Log("That direction didn't work! Go the other way!");
-	//
-	//				destination = destination == leftDestination ? rightDestination : leftDestination;
-	//
-	//				if (NavMesh.SamplePosition(destination, out NavMeshHit hit2, fleeingSearchRange, NavMesh.AllAreas))
-	//				{
-	//					agent.SetDestination(hit2.position);
-	//					pathfindingState = PathfindingState.Fleeing;
-	//				}
-	//				else
-	//				{
-	//					// You're getting cornered over here! Get as close to the wall as you can!
-	//					Debug.Log("Quick! Into that corner!");
-	//					Vector3 crammingDestination = transform.position - playerDirection * fleeingSearchRange;
-	//					if (NavMesh.SamplePosition(crammingDestination, out NavMeshHit hit3, fleeingSearchRange, NavMesh.AllAreas))
-	//					{
-	//						agent.SetDestination(hit3.position);
-	//						pathfindingState = PathfindingState.Fleeing;
-	//					}
-	//					else
-	//					{
-	//						Debug.Log("Break through!!!");
-	//						// If you're already cornered, make a run for it and break through!
-	//						Vector3 finalDirection = Quaternion.AngleAxis(180 + Random.Range(-30, 31), Vector3.up) * playerDirection;
-	//						Vector3 finalDestination = transform.position - (finalDirection * (AverageSightDist() + fleeingSearchRange));
-	//						if (NavMesh.SamplePosition(finalDestination, out NavMeshHit hit4, fleeingSearchRange, NavMesh.AllAreas))
-	//						{
-	//							agent.SetDestination(hit4.position);
-	//							pathfindingState = PathfindingState.Fleeing;
-	//						}
-	//					}
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
-
-	//private void UpdateAlertAI()
-	//{
-	//	if (stats.special == Ability.Alert)
-	//	{
-	//		foreach (Collider hitCollider in Physics.OverlapSphere(transform.position, alertDistance))
-	//		{
-	//			if (hitCollider.TryGetComponent(out WanderAI ai) && ai.IsAggressive)
-	//				ai.IncrementAlertness(100);
-	//		}
-	//	}
-	//
-	//	switch (stats.behavior)
-	//	{
-	//		case AIType.Prey:
-	//			PreyAI();
-	//			break;
-	//		case AIType.Projectile:
-	//			PredatorAI(); //TEMP
-	//			break;
-	//		case AIType.Charge:
-	//			PredatorAI(); //TEMP
-	//			break;
-	//		case AIType.Grapple:
-	//			PredatorAI(); //TEMP
-	//			break;
-	//		case AIType.Slam:
-	//			PredatorAI(); //TEMP
-	//			break;
-	//		default:
-	//			break;
-	//	}
-	//}
-
-	//private void UpdateWanderAI()
-	//{
-	//	// Non-Alerted Behavior
-	//	if (agent.remainingDistance <= agent.stoppingDistance && Alertness < stats.alertnessThreshold1) //done with path
-	//	{
-	//		if (RandomPoint(transform.position, wanderRadius, out Vector3 point)) //pass in our centre point and radius of area
-	//		{
-	//			agent.SetDestination(point);
-	//			pathfindingState = PathfindingState.Wandering;
-	//		}
-	//	}
-	//	else
-	//	{
-	//		// Stage 1 of Alerted Behavior
-	//		if ((agent.remainingDistance <= agent.stoppingDistance || pathfindingState != PathfindingState.Looking) && Alertness >= stats.alertnessThreshold1 && Alertness < stats.alertnessThreshold2)
-	//		{
-	//			if (RandomPoint(player.position, investigateRadius, out Vector3 point)) //pass in our centre point and radius of area
-	//			{
-	//				agent.SetDestination(point);
-	//				pathfindingState = PathfindingState.Looking;
-	//			}
-	//		}
-	//		else
-	//		{
-	//			// Stage 2 of Alerted Behavior behaviorType
-	//			if ((agent.remainingDistance <= agent.stoppingDistance || (pathfindingState != PathfindingState.Investigating && pathfindingState != PathfindingState.Fleeing)) && Alertness >= stats.alertnessThreshold2 && Alertness < stats.alertnessThreshold3)
-	//			{
-	//				if (stats.behavior == AIType.Prey)
-	//				{
-	//					PreyAI();
-	//				}
-	//				else
-	//				{
-	//					agent.SetDestination(player.position);
-	//					pathfindingState = PathfindingState.Investigating;
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
-
-	//private void AttackPlayer()
-	//{
-	//	if (timeSinceLastAttack >= shootable.FurnitureSO.attackInterval)
-	//	{
-	//		timeSinceLastAttack = 0f;
-	//		HuntingManager.Instance.DealDamageToPlayer(shootable.FurnitureSO.damage);
-	//	}
-	//	else
-	//	{
-	//		timeSinceLastAttack += Time.deltaTime;
-	//	}
-	//}
 }
