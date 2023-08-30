46b6dad65c978821b7745e44771afd16be3a14ee
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-08-30 21:44:55 +1000

    Added InvestigateBehaviour

--- Assets/Scripts/Furnitures/Shootable/Behaviours.cs
+++ Assets/Scripts/Furnitures/Shootable/Behaviours.cs
@@ -129,3 +129,26 @@ public class FleePlayerBehaviour : Behaviour
 		}
 	}
 }
+
+[CreateAssetMenu(fileName = "Investigate Behaviour", menuName = "Behaviours/Investigate")]
+public class InvestigateBehaviour : Behaviour
+{
+	public override void Act(ref Knowledge knowledge)
+	{
+		// Update dangerPosition if required
+		if (knowledge.CanSeePlayer)
+		{
+			knowledge.dangerPosition = knowledge.PlayerPosition;
+		}
+		else if (knowledge.MostProminentSound != null)
+		{
+			knowledge.dangerPosition = ((SoundAlert)knowledge.MostProminentSound).position;
+		}
+
+		// If we reached the end of our current path or a new sound has been detected, generate a new path
+		if (knowledge.Agent.remainingDistance < 1 && WanderAI.RandomPoint((Vector3)knowledge.dangerPosition, 2, out Vector3 investigatePosition))
+		{
+			knowledge.Agent.SetDestination(investigatePosition);
+		}
+	}
+}
