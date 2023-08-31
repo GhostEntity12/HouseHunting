e9f8f5d053cba18c009d5d94a4ba419d2a88d249
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-08-31 19:26:56 +1000

    Update InvestigateBehaviour.cs
    Implemented new Entry and Exit functions due to instanced behaviours

--- Assets/Scripts/Scriptable Objects/InvestigateBehaviour.cs
+++ Assets/Scripts/Scriptable Objects/InvestigateBehaviour.cs
@@ -5,19 +5,24 @@ public class InvestigateBehaviour : AIBehaviour
 {
 	public override void Act(ref Knowledge knowledge)
 	{
+		Vector3? dangerPosition = null;
 		// Update dangerPosition if required
 		if (knowledge.CanSeePlayer)
 		{
-			knowledge.dangerPosition = knowledge.PlayerPosition;
+			dangerPosition = knowledge.PlayerPosition;
 		}
 		else if (knowledge.MostProminentSound != null)
 		{
-			knowledge.dangerPosition = ((SoundAlert)knowledge.MostProminentSound).position;
+			dangerPosition = ((SoundAlert)knowledge.MostProminentSound).position;
 		}
 		// If we reached the end of our current path or a new sound has been detected, generate a new path
-		if (knowledge.Agent.remainingDistance < 1 && WanderAI.RandomPoint((Vector3)knowledge.dangerPosition, 2, out Vector3 investigatePosition))
+		if (knowledge.Agent.remainingDistance < 1 && WanderAI.RandomPoint((Vector3)dangerPosition, 2, out Vector3 investigatePosition))
 		{
 			knowledge.Agent.SetDestination(investigatePosition);
 		}
 	}
+
+	public override void Entry(ref Knowledge knowledge) { }
+
+	public override void Exit(ref Knowledge knowledge) { }
 }
