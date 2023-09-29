1f3941fae16dec1e82767894ba2dfc67c849e5e7
Kai Siang Kao <103819380@student.swin.edu.au>
2023-09-29 14:29:53 +1000

    Fixed UI misalignment and settings page

--- Assets/Scripts/UI/SliderContainer.cs
+++ Assets/Scripts/UI/SliderContainer.cs
@@ -8,4 +8,15 @@ public class SliderContainer : MonoBehaviour
 	[field: SerializeField] public string Key { get; private set; }
 	[field: SerializeField] public Slider Slider { get; private set; }
 	[field: SerializeField] public TextMeshProUGUI SliderValueText { get; private set; }
+
+    private void Start()
+    {
+        float sliderValue = PlayerPrefs.GetFloat(Key, 0);
+        SliderValueText.text = sliderValue.ToString();
+        Slider.value = sliderValue;
+        Slider.onValueChanged.AddListener(value =>
+        {
+            SliderValueText.text = value.ToString();
+        });
+    }
 }
