e8728e44b3b0a138b25d5422e4cdb97ba12afebf
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-10-04 02:52:46 +1100

    Fixed issues with jittery AI

--- Assets/Scripts/Scriptable Objects/RoamBehaviour.cs
+++ Assets/Scripts/Scriptable Objects/RoamBehaviour.cs
@@ -6,11 +6,11 @@ public class RoamBehaviour : AIBehaviour
 	public override void Act(ref Knowledge knowledge)
 	{
 		//If lure isn't active then roam normally
-		if (knowledge.LurePosition == Vector3.zero)
-		{
+		//if (knowledge.LurePosition == Vector3.zero)
+		//{
 			// If the agent is close to it's destination, choose a new one
-			//if (knowledge.Agent.remainingDistance < 1)
-			//{
+			if (knowledge.Agent.remainingDistance < 1)
+			{
 				// Get a point in front of the furniture, and a random point
 				// in a circle centered on that point, then navigate to it
 				// This favours the AI moving generally forward
@@ -22,14 +22,14 @@ public class RoamBehaviour : AIBehaviour
 				{
 					knowledge.Agent.SetDestination(destination);
 				}
-			//}
-		}
+			}
+		//}
 
 		//if lure is active then set destination for lure
-		else
-		{
-			knowledge.Agent.SetDestination(knowledge.LurePosition);
-		}
+		//else
+		//{
+		//	knowledge.Agent.SetDestination(knowledge.LurePosition);
+		//}
 	}
 	public override void Entry(ref Knowledge knowledge)
 	{
