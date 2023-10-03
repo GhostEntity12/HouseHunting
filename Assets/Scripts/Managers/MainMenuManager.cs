ff0ebd4ddaeccd5406bad0484ce2cb12a756df3e
Waltzerr <riptidew@gmail.com>
2023-10-03 20:01:32 +1100

    Implemented the cinematic to play on a new game

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,18 +1,49 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MainMenuManager : Singleton<MainMenuManager>
 {
     [SerializeField] CanvasGroup settingsGroup;
-	private void Start() 
+    [SerializeField] VideoPlayer videoPlayer; // Assign this through the inspector
+    [SerializeField] RawImage videoOutput; // Assign this through the inspector
+    private bool isPlayingCinematic = false; // Flag to check if cinematic is playing
+
+    private void Start()
     {
-		AudioManager.Instance.Play("Ambience02");
+        AudioManager.Instance.Play("Ambience02");
+        if (videoPlayer == null || videoOutput == null)
+        {
+            Debug.LogError("VideoPlayer or VideoOutput not set.");
+            return;
+        }
+        videoPlayer.loopPointReached += OnCinematicEnded; // Add event for when video ends
+        videoOutput.gameObject.SetActive(false); // Initially hide the video output
     }
 
     public void NewGame()
     {
         DataPersistenceManager.Instance.NewGame();
-        SceneManager.LoadScene(1);
+        PlayCinematic(); // Play the cinematic when New Game is selected
+    }
+
+    private void PlayCinematic()
+    {
+        AudioManager.Instance.Pause("Ambience02");
+        videoOutput.gameObject.SetActive(true); // Enable video output to show the video
+        videoPlayer.Play(); // Play the cinematic
+        isPlayingCinematic = true; // Set flag to true
+    }
+
+    private void OnCinematicEnded(VideoPlayer vp)
+    {
+        if (isPlayingCinematic) // Check if currently playing cinematic
+        {
+            isPlayingCinematic = false; // Reset flag
+            videoOutput.enabled = false; // Disable the video output
+            SceneManager.LoadScene(1); // Load the next scene after cinematic ends
+        }
     }
 
     public void Continue()
@@ -20,7 +51,6 @@ public class MainMenuManager : Singleton<MainMenuManager>
         DataPersistenceManager.Instance.LoadGame();
         SceneManager.LoadScene(2);
     }
-
 
     public void SetSettingsVisible(bool visible)
     {
