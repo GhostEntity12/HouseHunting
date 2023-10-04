3fcaa338e1a086ae8e7f4e17defea96c91d2aba8
Kai Siang Kao <103819380@student.swin.edu.au>
2023-10-05 10:02:25 +1100

    Fixed inventory not closing after selecting placeable

--- Assets/Scripts/Managers/HouseInputManager.cs
+++ Assets/Scripts/Managers/HouseInputManager.cs
@@ -39,18 +39,6 @@ public class HouseInputManager : Singleton<HouseInputManager>
 
 	private void OnDestroy() => playerInput.Dispose();
 
-	//public void SetInventoryAvailability(bool available)
-	//{
-	//	if (available)
-	//	{
-	//		playerInput.Inventory.Enable();
-	//	}
-	//	else
-	//	{
-	//		playerInput.Inventory.Disable();
-	//	}
-	//}
-
 	/// <summary>
 	/// Removes the selected placeable from the scene and returns it to the inventory
 	/// </summary>
@@ -60,6 +48,5 @@ public class HouseInputManager : Singleton<HouseInputManager>
 
 		GameManager.Instance.PermanentInventory.AddItem(HouseManager.Instance.HoldingPlaceable.InventoryItem);
 		Destroy(HouseManager.Instance.HoldingPlaceable.gameObject);
-		//SetInventoryAvailability(true);
 	}
 }
