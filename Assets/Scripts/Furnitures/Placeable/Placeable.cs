dfe9577f621a68aa191a74b5b6e4930fd8dc423b
GhostEntity12 <48895274+GhostEntity12@users.noreply.github.com>
2023-10-04 16:20:52 +1100

    Tweak to cache value rather than obtaining it each time

--- Assets/Scripts/Furnitures/Placeable/Placeable.cs
+++ Assets/Scripts/Furnitures/Placeable/Placeable.cs
@@ -18,13 +18,15 @@ public class Placeable : MonoBehaviour, IInteractable
     public MeshCollider ChildMeshCollider => childMeshCollider;
     public bool CanPlaceOnSurface => canPlaceOnSurface;
     public bool Interactable => HouseManager.Instance.HoldingPlaceable == null;
-    public string InteractActionText => "Pickup " + DataPersistenceManager.Instance.AllFurnitureSO.First(i => i.id == InventoryItem.id).name;
+    private string furnitureName;
+    public string InteractActionText => $"Pickup {furnitureName}";
 
 
     private void Start()
     {
         childMeshCollider = GetComponentInChildren<MeshCollider>();
-    }
+        furnitureName = DataPersistenceManager.Instance.AllFurnitureSO.First(i => i.id == InventoryItem.id).name;
+	}
 
     private void OnTriggerExit(Collider other) 
     {
